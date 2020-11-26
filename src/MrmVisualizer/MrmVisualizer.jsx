abaeff0378d31b9bb373e4a27742f6a81b604733
Eric Wang <ew9@williams.edu>
2020-11-25 19:09:21 -0800

    first commit

---
+++ src/MrmVisualizer/MrmVisualizer.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class MrmVisualizer extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <> 
+            <div> 
+                Helloasdfs
+            </div> 
+            </> 
+        );
+    }
+}
